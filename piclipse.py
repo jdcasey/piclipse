40769ada2c869e1f9b45999543fa3e0d4fa9d16d
John Casey <jdcasey@commonjava.org>
2017-08-13 18:42:14 +0000

    cleaning up button press logic and adding capture button

--- piclipse.py
+++ piclipse.py
@@ -5,10 +5,12 @@ from time import sleep
 import picamera
 
 RECORD_SW=22
-RECORD_LED=21
+CAPTURE_SW=27
+RECORD_LED=17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RECORD_SW, GPIO.IN)
+GPIO.setup(CAPTURE_SW, GPIO.IN)
 GPIO.setup(RECORD_LED, GPIO.OUT)
 
 def capture_picture(path='/tmp/test.jpg'):
@@ -27,15 +29,27 @@ def capture_video(path='/tmp/test.h264'):
 
 #capture_video()
 
+recording=False
+on=False
 try:
+    print "piclipse is ready"
     while True:
         if GPIO.input(RECORD_SW):
-            print "Recording started"
+            print "Recording button press"
+            recording=not recording
+            on = recording
+            GPIO.output(RECORD_LED, on)
+            sleep(2)
+        elif recording:
+            on = not on
+            GPIO.output(RECORD_LED, on)
+        elif not recording and GPIO.input(CAPTURE_SW):
+            print "Capture button press"
             GPIO.output(RECORD_LED, 1)
-        else:
-            print "Recording released"
+            sleep(5)
             GPIO.output(RECORD_LED, 0)
-        sleep(0.1)
+            on = False
+        sleep(0.25)
 finally:
     GPIO.cleanup()
 
